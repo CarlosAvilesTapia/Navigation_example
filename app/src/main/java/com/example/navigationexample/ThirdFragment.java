1d0d813f61cd50be475627d9acbcc93c816b1779
CAT <cavilest@gmail.com>
2023-07-06 14:55:39 -0400

    Ejercicio individual 4. - Se ordena el diseño

--- app/src/main/java/com/example/navigationexample/ThirdFragment.java
+++ app/src/main/java/com/example/navigationexample/ThirdFragment.java
@@ -1,15 +1,13 @@
 package com.example.navigationexample;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.navigationexample.databinding.FragmentSecondBinding;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import com.example.navigationexample.databinding.FragmentThirdBinding;
 
 /**
